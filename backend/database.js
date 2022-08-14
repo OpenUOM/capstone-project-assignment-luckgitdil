7bbd9c3ddbef33b1da74ba137d09e605f29328a4
luckgitdil <sjothiy@gmail.com>
2022-08-15 00:33:48 +0530

    database

--- backend/database.js
+++ backend/database.js
@@ -37,7 +37,7 @@ const readTeachers = async () => {
 }
 
 const readTeacherInfo = async (id) => {
-    const sql = `SELECT * FROM teacher`
+    const sql = `SELECT * FROM dsfsd`
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql)
