bd36f9df23ad85d6da7443d284c4647cc6164793
luckgitdil <sjothiy@gmail.com>
2022-08-16 20:27:26 +0530

    18

--- backend/database.js
+++ backend/database.js
@@ -124,7 +124,7 @@ const addStudent = async (id, name, age, hometown) => {
     const sql = `INSERT INTO student(id,name,age,hometown) values (?,?,?,?)`
     return new Promise((resolve, reject) => {
         knex_db
-            .raw(sql[id, name, age, hometown])
+            .raw(sql,[id, name, age, hometown])
             .then(() => {
                 resolve({status: "Successfully inserted Student"});
             })
