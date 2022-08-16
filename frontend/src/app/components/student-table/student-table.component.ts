94a4c8cf33e5105678bcd0c676ed9555935df657
luckgitdil <sjothiy@gmail.com>
2022-08-16 16:59:48 +0530

    16

--- frontend/src/app/components/student-table/student-table.component.ts
+++ frontend/src/app/components/student-table/student-table.component.ts
@@ -34,13 +34,6 @@ export class StudentTableComponent implements OnInit {
     this.router.navigate(['editStudent'], navigationExtras )
   }
 
-  initializeDB(){
-    this.service.initializeDB().subscribe((response) => {
-      console.log('DB is Initialized')
-    }, (error) => {
-      console.log('ERROR - ', error)
-    })
-  }
   getStudentData(){
     this.selected = 'Students';
     this.service.getStudentData().subscribe((response)=>{
